0354c5c711c0613ff1df458bbd5bc5cbb26cd196
EmanuelCamejo <emakme@gmail.com>
2021-10-04 09:17:25 -0300

    Sobo tambien el tp1 que habia entregado

---
+++ TP1/Emanuel.Camejo.2D.TP1/MiCalculadora/FormCalculadora.Designer.cs
@@ -0,0 +1,189 @@
+﻿
+namespace MiCalculadora
+{
+    partial class FormCalculadora
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbOperador = new System.Windows.Forms.ComboBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lstOperaciones = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // cmbOperador
+            // 
+            this.cmbOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperador.FormattingEnabled = true;
+            this.cmbOperador.Location = new System.Drawing.Point(215, 107);
+            this.cmbOperador.Name = "cmbOperador";
+            this.cmbOperador.Size = new System.Drawing.Size(141, 23);
+            this.cmbOperador.Sorted = true;
+            this.cmbOperador.TabIndex = 2;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.AccessibleDescription = "txtNum2";
+            this.txtNumero2.Location = new System.Drawing.Point(374, 107);
+            this.txtNumero2.Multiline = true;
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(140, 36);
+            this.txtNumero2.TabIndex = 3;
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.AccessibleDescription = "txtNum1";
+            this.txtNumero1.Location = new System.Drawing.Point(58, 107);
+            this.txtNumero1.Multiline = true;
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(140, 36);
+            this.txtNumero1.TabIndex = 1;
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.AccessibleDescription = "btnOperar";
+            this.btnOperar.Location = new System.Drawing.Point(58, 157);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(140, 36);
+            this.btnOperar.TabIndex = 4;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.AccessibleDescription = "btnLimpiar";
+            this.btnLimpiar.Location = new System.Drawing.Point(215, 157);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(140, 36);
+            this.btnLimpiar.TabIndex = 5;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.AccessibleDescription = "btnCerrar";
+            this.btnCerrar.Location = new System.Drawing.Point(374, 157);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(140, 36);
+            this.btnCerrar.TabIndex = 6;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnConvertirABinario
+            // 
+            this.btnConvertirABinario.AccessibleDescription = "btnConvertirABinario";
+            this.btnConvertirABinario.Location = new System.Drawing.Point(58, 209);
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.Size = new System.Drawing.Size(225, 39);
+            this.btnConvertirABinario.TabIndex = 7;
+            this.btnConvertirABinario.Text = "Convertir a Binario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            this.btnConvertirABinario.Click += new System.EventHandler(this.btnConvertirABinario_Click);
+            // 
+            // btnConvertirADecimal
+            // 
+            this.btnConvertirADecimal.AccessibleDescription = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Location = new System.Drawing.Point(289, 209);
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Size = new System.Drawing.Size(225, 39);
+            this.btnConvertirADecimal.TabIndex = 8;
+            this.btnConvertirADecimal.Text = "Converti a Decimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            this.btnConvertirADecimal.Click += new System.EventHandler(this.btnConvertirADecimal_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AccessibleDescription = "lblResultado";
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Location = new System.Drawing.Point(445, 67);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 15);
+            this.lblResultado.TabIndex = 9;
+            // 
+            // lstOperaciones
+            // 
+            this.lstOperaciones.FormattingEnabled = true;
+            this.lstOperaciones.ItemHeight = 15;
+            this.lstOperaciones.Location = new System.Drawing.Point(546, 67);
+            this.lstOperaciones.Name = "lstOperaciones";
+            this.lstOperaciones.Size = new System.Drawing.Size(222, 304);
+            this.lstOperaciones.TabIndex = 9;
+            // 
+            // FormCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstOperaciones);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.cmbOperador);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FormCalculadora";
+            this.Text = "Calculadora de Emanuel Camejo 2°D";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.btnCerrar_Click);
+            this.Load += new System.EventHandler(this.FrmCalculadora_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbOperador;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.ListBox lstOperaciones;
+
+       
+    }
+}
+
---
+++ TP1/Emanuel.Camejo.2D.TP1/MiCalculadora/FormCalculadora.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Entidades;
+
+namespace MiCalculadora
+{
+    public partial class FormCalculadora : Form
+    {
+        public FormCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        
+      
+        public void Limpiar()
+        {
+            txtNumero1.Clear();
+            txtNumero2.Clear();
+            cmbOperador.SelectedIndex = -1;
+            lstOperaciones.ClearSelected();
+        }
+
+
+        /// <summary>
+        /// Creo dos intancias de la clase Operando y les asigno los atributos recibidos en el metodo, luego utilizo el metodo
+        /// Operar de la clase calculadora para hacer la operación
+        /// </summary>
+        /// <param name="numero1">parametro de tipo string que representa a una de los números que se utilizaran para el calculo</param>
+        /// <param name="numero2">parametro de tipo string que representa a una de los números que se utilizaran para el calculo</param>
+        /// <param name="operador">parametro de tipo string que representa a tipo de operación</param>
+        /// <returns>Retora un double con el resultado de la operacion</returns>
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            Operando  operando1 = new Operando(numero1);
+            Operando  operando2 = new Operando(numero2);
+            double resultado = Calculadora.Operar(operando1, operando2, char.Parse(operador));
+            return resultado;
+        }
+
+
+        #region Eventos
+
+        private void FrmCalculadora_Load(object sender, EventArgs e)
+        {
+            
+            cmbOperador.Items.Add('+');
+            cmbOperador.Items.Add('-');
+            cmbOperador.Items.Add('*');
+            cmbOperador.Items.Add('/');
+            cmbOperador.SelectedIndex = 0;
+            Limpiar();
+        }
+
+        
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            DialogResult resultado = MessageBox.Show("¿Seguro de querer salir?","Salir", MessageBoxButtons.YesNo,MessageBoxIcon.Question);
+            if (resultado==DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+        private void FormCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+
+        /// <summary>
+        /// Utilizo el metrodo Operar y le paso lo que se ingreso de los TexBox y de BomboBox para hacer el calculo.
+        /// Luego lo muestro en el label y el listBox
+        /// </summary>       
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resultado = Operar(txtNumero1.Text, txtNumero2.Text, cmbOperador.SelectedItem.ToString());
+            lblResultado.Text = resultado.ToString();
+            lstOperaciones.Items.Add($"{txtNumero1.Text} {cmbOperador.SelectedItem} {txtNumero2.Text} = {resultado.ToString()}");
+        }
+
+        /// <summary>
+        /// Creo un instancia de la clase Operando y utlilizo el metodo DecimalBinario para convertirlo a binario, el cual le paso como parametro lo ingresado en el 1er TexBox
+        /// Luego lo muestro en el label y en el ListBox
+        /// </summary>
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Operando numero1 = new Operando();
+            string resultadoDecimalABinario = numero1.DecimalBinario(txtNumero1.Text);
+            lblResultado.Text = resultadoDecimalABinario;
+            lstOperaciones.Items.Add($"Convertir {txtNumero1.Text} a Binario: {resultadoDecimalABinario}");
+        }
+
+        /// <summary>
+        /// Creo un instancia de la clase Operando y utlilizo el metodo BinarioDecimal para convertirlo a Decimal, el cual le paso como parametro lo ingresado en el 1er TexBox
+        /// Luego lo muestro en el label y en el ListBox
+        /// </summary>
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Operando numero1 = new Operando();
+            string resultadoBinarioADecimal = numero1.BinarioDecimal(txtNumero1.Text);
+            lblResultado.Text = resultadoBinarioADecimal;
+            lstOperaciones.Items.Add($"Convertir {txtNumero1.Text} a Decimal: { resultadoBinarioADecimal}");
+        }
+
+        #endregion
+
+        
+    }
+}

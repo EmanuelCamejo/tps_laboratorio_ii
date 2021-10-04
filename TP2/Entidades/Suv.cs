0667a9f4a5a0c552f01819346299ee53c62fa534
EmanuelCamejo <emakme@gmail.com>
2021-10-04 08:54:42 -0300

    Lo subo terminado

---
+++ TP2/Entidades/Suv.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Suv:Vehiculo
+    {
+
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Suv(EMarca marca, string chasis, ConsoleColor color)
+            : base(chasis, marca, color)
+        {
+        }
+        /// <summary>
+        /// SUV son 'Grande'
+        /// </summary>
+        protected ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Grande;
+            }
+        }
+
+        /// <summary>
+        /// Sobreescribo el metodo mostrar de la clase base, donde imprimo los datos del Suv
+        /// </summary>
+        /// <returns>Retorna un string con los datos del Suv</returns>
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SUV");
+            sb.AppendLine($"{base.Mostrar()}");
+            sb.AppendLine("---------------------");
+            sb.AppendLine("");
+            sb.AppendLine($"TAMAÑO : {this.Tamanio}");
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
